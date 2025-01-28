d4ca5371fb62779fccb50355a92b05067a1fa1b9
Steven Thompson <s.r.thompson@ljmu.ac.uk>
2025-01-28 14:59:04 +0000

    NamesList

---
+++ names.py
@@ -0,0 +1,8 @@
+names = ['ged', 'trev', 'paul', 'ste'] 
+print (names)
+print(names[0])
+print(names[0].title())
+print(names[1])
+print(names[3])
+message = f"Good day {names[0].title()}."
+print(message)
